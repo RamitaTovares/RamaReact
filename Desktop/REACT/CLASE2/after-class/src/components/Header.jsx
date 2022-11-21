ec5c2b0d8176d59d522aab243093145af68aa5e8
RamitaTovares <ramiro.tovares@gmail.com>
2022-11-21 14:41:25 -0300

    ultima modif

--- Desktop/REACT/CLASE2/after-class/src/components/Header.jsx
+++ Desktop/REACT/CLASE2/after-class/src/components/Header.jsx
@@ -9,9 +9,6 @@ const Header = () => {
                 <div className="col-md-12 .bg-info.bg-gradient">
                     <NavBar />
                 </div>
-                <div className="col-md-6 d-flex align-items-center justify-content-end">
-                    <CartWidget />
-                </div>
             </div>
         </div>
     )
